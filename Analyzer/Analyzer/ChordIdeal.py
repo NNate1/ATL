dc6032b3560e768686d0c90458840240d15daf56
nnate1 <nuno.policarpo@tecnico.ulisboa.pt>
2025-01-29 20:31:41 +0000

    add null successor to joining Chord nodes

--- Analyzer/Analyzer/ChordIdeal.py
+++ Analyzer/Analyzer/ChordIdeal.py
@@ -30,26 +30,28 @@ class Pointer:
         return self.__str__()
 
 
-# Circular order
+# Circular Order
 def between(a: str, b: str, c: str) -> bool:
     if a == c:
         return True
 
-    if a == b:
-        return False
+    # if a == b:
+    #     return False
 
     if a < c:
         return a < b and b <= c
 
-    return a <= b or b <= c
-
+    return a < b or b <= c
 
 def is_ideal(pointers: dict[str, Pointer], current_members: list[str]) -> bool:
 
     for i in range(0, len(current_members)):
 
-        if current_members[i] not in pointers:
-            return False
+
+        assert current_members[i] in pointers, f"Node {current_members[i]} not in pointers, pointers:\n{pointers}"
+        # if current_members[i] not in pointers:
+        #     # pointers[current_members[i]] = Pointer(current_members[i], None)
+        #     return False
 
         is_not_successor = pointers[current_members[i]].succ != current_members[(i + 1) % len(current_members)]
         is_not_only_node = len(current_members) != 1 or pointers[current_members[i]].succ is not None
@@ -58,6 +60,59 @@ def is_ideal(pointers: dict[str, Pointer], current_members: list[str]) -> bool:
             return False
 
     return True
+
+def update_responsibilites(pointers: dict[str, Pointer], all_keys: set[str], ongoing_responsibilities: dict[str, set[str]], responsible_intervals: OrderedDict[str, ResponsibleStart | ResponsibleEnd], time: str) -> dict[str, set[str]]:
+    new_responsibilities = {}
+    for pointer in pointers.values():
+        node = pointer.node
+        succ = pointer.succ
+
+        if succ is None:
+
+            # print(f"Node {node} is responsible for all keys")
+            new_responsibilities[node] = all_keys
+            continue
+
+        new_keys = new_responsibilities.get(succ, set())
+        new_responsibilities[succ] = new_keys
+
+
+
+        # print(f"Node {node}, succ {succ}")
+        for key in all_keys:
+            # print(f"Checking key {key}")
+            if between(node, key, succ):
+                # print(f"Adding key {key} to {succ}")
+                new_keys.add(key)
+
+
+
+    for pointer in pointers.values():
+        node = pointer.succ
+
+        if node is None:
+            node = pointer.node
+
+        prev_keys = ongoing_responsibilities.get(node, set())
+        new_keys = new_responsibilities.get(node, set())
+
+        # print(pointer.node, pointer.succ)
+        # pprint(new_keys)
+
+        if new_keys != prev_keys:
+            # print(f"Node {node} keys: {new_keys},\nold keys: {prev_keys}")
+            for r_start in reversed(responsible_intervals.values()):
+                if type(r_start) == ResponsibleStart and r_start.node == node:
+                    # print(f"End {r_start.get_id()} at {time}")
+                    r_start.set_end_time(time)
+                    r_end = ResponsibleEnd(node, r_start.get_keys(), time, r_start.id)
+                    responsible_intervals["End-" + r_end.get_id()] = r_end
+                    break
+
+            r_start = ResponsibleStart(node, new_keys, time, str(len(responsible_intervals)))
+            responsible_intervals[r_start.get_id()] = r_start
+
+    return new_responsibilities
 
 def get_ideal_responsible(ideal_log: Path, limit_time: str, all_keys: set[str], operations: OrderedDict[str, Operation], member_intervals: OrderedDict[str, MemberStart| MemberEnd]) -> tuple[dict, dict]:
 
@@ -83,7 +138,7 @@ def get_ideal_responsible(ideal_log: Path, limit_time: str, all_keys: set[str], 
     with open(ideal_log, 'r') as f:
         for line in f:
             time, _, member, succ = line.strip().split(', ')
-
+            
             ## Infer current members
             while member_iter < len(member_list) and member_list[member_iter].get_time() < time:
                 membership_node = member_list[member_iter].get_node()
@@ -97,13 +152,22 @@ def get_ideal_responsible(ideal_log: Path, limit_time: str, all_keys: set[str], 
                     assert not (pos < len(current_members) and current_members[pos] == membership_node), f"Node {member_list[member_iter].get_node()} is already a member, current members:\n{current_members}"
                     current_members.insert(pos, membership_node)
 
+                    if membership_node not in pointers:
+                        # print(f"Adding {membership_node} to pointers")
+                        pointers[membership_node] = Pointer(membership_node, None)
 
 
+
+
+                member_time = member_list[member_iter].get_time()
+
+                ## Update responsibilities
+                ongoing_responsibilities = update_responsibilites(pointers, all_keys, ongoing_responsibilities, responsible_intervals, member_time)
+
+                ## Update ideal states
                 is_ideal_state = is_ideal(pointers, current_members)
 
-
                 if is_ideal_state != (ongoing_ideal is not None):
-                    member_time = member_list[member_iter].get_time()
 
                     if ongoing_ideal is None:
                         ongoing_ideal = IdealStart(member_time, str(len(ideal_states)))
@@ -117,7 +181,10 @@ def get_ideal_responsible(ideal_log: Path, limit_time: str, all_keys: set[str], 
 
                         ongoing_ideal = None
 
+
+
                 member_iter += 1
+
 
 
             if time > limit_time:
@@ -136,60 +203,61 @@ def get_ideal_responsible(ideal_log: Path, limit_time: str, all_keys: set[str], 
             # pprint(pointers)
 
             #### Responsible
-            new_responsibilities = {}
-            for pointer in pointers.values():
-                node = pointer.node
-                succ = pointer.succ
-
-                if succ is None:
-
-                    # print(f"Node {node} is responsible for all keys")
-                    new_responsibilities[node] = all_keys
-                    continue
-
-                new_keys = new_responsibilities.get(succ, set())
-                new_responsibilities[succ] = new_keys
-
-
-
-                # print(f"Node {node}, succ {succ}")
-                for key in all_keys:
-                    # print(f"Checking key {key}")
-                    if between(node, key, succ):
-                        # print(f"Adding key {key} to {succ}")
-                        new_keys.add(key)
-
-
-
-            for pointer in pointers.values():
-                node = pointer.succ
-
-                if node is None:
-                    node = pointer.node
-
-                prev_keys = ongoing_responsibilities.get(node, set())
-                new_keys = new_responsibilities.get(node, set())
-
-                # print(pointer.node, pointer.succ)
-                # pprint(new_keys)
-
-                if new_keys != prev_keys:
-                    # print(f"Node {node} keys: {new_keys},\nold keys: {prev_keys}")
-                    for r_start in reversed(responsible_intervals.values()):
-                        if type(r_start) == ResponsibleStart and r_start.node == node:
-                            # print(f"End {r_start.get_id()} at {time}")
-                            r_start.set_end_time(time)
-                            r_end = ResponsibleEnd(node, r_start.get_keys(), time, r_start.id)
-                            responsible_intervals["End-" + r_end.get_id()] = r_end
-                            break
-
-                    r_start = ResponsibleStart(node, new_keys, time, str(len(responsible_intervals)))
-                    responsible_intervals[r_start.get_id()] = r_start
-
-
-
-
-            ongoing_responsibilities = new_responsibilities
+            ongoing_responsibilities = update_responsibilites(pointers, all_keys, ongoing_responsibilities, responsible_intervals, time)
+            # new_responsibilities = {}
+            # for pointer in pointers.values():
+            #     node = pointer.node
+            #     succ = pointer.succ
+            #
+            #     if succ is None:
+            #
+            #         # print(f"Node {node} is responsible for all keys")
+            #         new_responsibilities[node] = all_keys
+            #         continue
+            #
+            #     new_keys = new_responsibilities.get(succ, set())
+            #     new_responsibilities[succ] = new_keys
+            #
+            #
+            #
+            #     # print(f"Node {node}, succ {succ}")
+            #     for key in all_keys:
+            #         # print(f"Checking key {key}")
+            #         if openChord_between(node, key, succ):
+            #             # print(f"Adding key {key} to {succ}")
+            #             new_keys.add(key)
+            #
+            #
+            #
+            # for pointer in pointers.values():
+            #     node = pointer.succ
+            #
+            #     if node is None:
+            #         node = pointer.node
+            #
+            #     prev_keys = ongoing_responsibilities.get(node, set())
+            #     new_keys = new_responsibilities.get(node, set())
+            #
+            #     # print(pointer.node, pointer.succ)
+            #     # pprint(new_keys)
+            #
+            #     if new_keys != prev_keys:
+            #         # print(f"Node {node} keys: {new_keys},\nold keys: {prev_keys}")
+            #         for r_start in reversed(responsible_intervals.values()):
+            #             if type(r_start) == ResponsibleStart and r_start.node == node:
+            #                 # print(f"End {r_start.get_id()} at {time}")
+            #                 r_start.set_end_time(time)
+            #                 r_end = ResponsibleEnd(node, r_start.get_keys(), time, r_start.id)
+            #                 responsible_intervals["End-" + r_end.get_id()] = r_end
+            #                 break
+            #
+            #         r_start = ResponsibleStart(node, new_keys, time, str(len(responsible_intervals)))
+            #         responsible_intervals[r_start.get_id()] = r_start
+            #
+            #
+            #
+            #
+            # ongoing_responsibilities = new_responsibilities
 
 
             #### Ideal
